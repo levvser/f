e6f1cd8b0533f67577b3aaa08b1f0a1ec7c915ee
levvser <vval.mail@icloud.com>
2024-08-21 17:51:30 +0200

    +

--- src/modules/products/components/product-actions/index.tsx
+++ src/modules/products/components/product-actions/index.tsx
@@ -171,10 +171,10 @@ export default function ProductActions({
           data-testid="add-product-button"
         >
           {!variant
-            ? "Select variant"
+            ? "Selezionare una variante"
             : !inStock
-            ? "Out of stock"
-            : "Add to cart"}
+            ? "Non disponibile"
+            : "Aggiungi al carrello"}
         </Button>
         <MobileActions
           product={product}
