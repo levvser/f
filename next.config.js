d275318d10396e916b970c19dd9e8a10dfe4912b
levvser <vval.mail@icloud.com>
2024-10-21 21:55:18 +0200

    +

--- next.config.js
+++ next.config.js
@@ -25,14 +25,9 @@ const nextConfig = withStoreConfig({
         protocol: "https",
         hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
       },
-      {
-        protocol: "https",
-        hostname: "ac-bucket.fra1.digitaloceanspaces.com",
-      },
     ],
   },
 })
-
 
 console.log("next.config.js", JSON.stringify(module.exports, null, 2))
 
